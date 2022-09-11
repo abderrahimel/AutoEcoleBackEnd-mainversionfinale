c894e2b7dc7995a887271875106f817e5317998c
lucylefer <abderrahimelayadi7@gmail.com>
2022-09-12 01:06:51 +0200

    new commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-
+use App\Models\User;
 class UserFactory extends Factory
 {
     /**
@@ -12,11 +12,15 @@ class UserFactory extends Factory
      *
      * @return array
      */
+    protected $model = User::class;
+
     public function definition()
     {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'type' => 'adminAuto',
+            'login' => $this->faker->name(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
